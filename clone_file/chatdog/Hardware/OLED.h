068f980b962187f6bcaee617e540e4be37d47d0f
kadsfvcui <1509797189@qq.com>
2024-04-09 18:37:42 +0800

    change3

---
+++ clone_file/chatdog/Hardware/OLED.h
@@ -0,0 +1,20 @@
+#ifndef __OLED_H
+#define __OLED_H
+
+void OLED_Init(void);
+void OLED_Clear(void);
+void OLED_ShowChar(uint8_t Line, uint8_t Column, char Char);
+void OLED_ShowString(uint8_t Line, uint8_t Column, char *String);
+void OLED_ShowNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);
+void OLED_ShowSignedNum(uint8_t Line, uint8_t Column, int32_t Number, uint8_t Length);
+void OLED_ShowHexNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);
+void OLED_ShowBinNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);
+void OLED_ShowChineseString(uint8_t Line, uint8_t Column, uint8_t Start, uint8_t End);
+void OLED_ShowPicture(uint8_t Line, uint8_t Column, uint8_t x, uint8_t y,uint8_t Start);
+void OLED_ShowGIF(uint8_t Num);
+void OLED_ShowMyPicture(uint8_t Line, uint8_t Column, uint8_t x, uint8_t y,uint8_t Start);
+void OLED_Show_eyes_round(void);
+void OLED_Show_eyes_crash(void);
+void OLED_Show_eyes_confot(void);
+
+#endif
---
+++ clone_file/chatdog/User/main.c
@@ -0,0 +1,25 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "Servo.h"
+#include "Serial.h"
+#include "Final.h"
+
+float Angle;
+uint16_t RxData;
+/*
+   01 前进；02 后退；03 左转；
+   04 右转；05 招手；06 摇摆；
+   07 坐下；08 趴下；09 停止；
+   */
+
+int main(void)
+{   
+	Final_Init();
+	Start_up();    //开机动画
+	
+	while(1)
+	{   
+         Final();
+	}
+}
